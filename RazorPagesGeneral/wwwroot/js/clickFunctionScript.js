7c315d31dcbc81c66b8a939dcf47193c99b1d95c
morning <matveygromykhalin@gmail.com>
2022-06-06 21:47:01 +0300

    Не перенес на бд ничег и даж видос не глянул( Чет устал, завтра первый день зачетной недели. Сегодня перенес блок ивента в отдельную вьюшку. Это сделать легко, а вот трахаться с логикой после этого переноса уже не так приятно, но в целом код стал приятней и экологичней. Не забудь про бд и баг(иногда не понятно почему просто удаляется картинка noImageCase.jpg)

---
+++ RazorPagesGeneral/wwwroot/js/clickFunctionScript.js
@@ -0,0 +1,24 @@
+﻿
+var logElemX = document.getElementById("X-coord");
+var logElemY = document.getElementById("Y-coord");
+
+
+var pointer_img = new Image();
+pointer_img.src = '/images/pointer.png';
+
+let tchbl = new Path2D();
+tchbl.rect(0, 0, 4000, 4000);
+ctx.stroke(tchbl);
+
+
+canvas.addEventListener('click', function (event) {
+    const windowInnerWidth = window.innerWidth
+    var kef = canvas.width / (windowInnerWidth - 260);
+    if (ctx.isPointInPath(tchbl, event.offsetX, event.offsetY)) {
+
+        ctx.drawImage(pointer_img, event.offsetX * kef - 20, event.offsetY * kef - 47, 40, 58)
+
+        logElemX.innerHTML = event.offsetX * kef - 20;
+        logElemY.innerHTML = event.offsetY * kef - 47;
+    }
+});
--- RazorPagesGeneral/wwwroot/js/map-script.js
+++ RazorPagesGeneral/wwwroot/js/map-script.js
@@ -1,9 +1,6 @@
 ﻿var c = document.getElementById("canvas");
 var ctx = c.getContext("2d");
 
-
-let tchbl = new Path2D();
-
 var village_img = new Image();
 village_img.src = document.getElementById("canvas").dataset.container;
 village_img.onload = drawImageActualSize;
@@ -11,43 +8,9 @@ village_img.onload = drawImageActualSize;
 var pointer_img = new Image();
 pointer_img.src = '/images/pointer.png';
 
-var logElemX = document.querySelector(".innerX");
-var logElemY = document.querySelector(".innerY");
-
 const count = document.getElementById("Main_script").dataset.container
 function drawImageActualSize() {
     canvas.width = this.naturalWidth;
     canvas.height = this.naturalHeight;
-    tchbl.rect(0, 0, 4000, 4000);
     ctx.drawImage(this, 0, 0, canvas.width, canvas.height);
-    ctx.stroke(tchbl);
-    for (var i = 0; i < count; i++) {
-        try {
-            X = document.getElementById(i + "X").innerHTML
-            Y = document.getElementById(i + "Y").innerHTML
-            if (X != "-1" && Y != "-1") {
-                ctx.drawImage(pointer_img, X, Y, 40, 58)
-            }
-        }
-        catch { continue }
-    }
-
-
 }
-
-
-canvas.addEventListener('click', function (event) {
-    const windowInnerWidth = window.innerWidth
-    var kef = canvas.width / (windowInnerWidth-260);
-    if (ctx.isPointInPath(tchbl, event.offsetX, event.offsetY)) {
-        ctx.drawImage(pointer_img, event.offsetX * kef - 20, event.offsetY * kef - 47, 40, 58)
-        //alert(windowInnerWidth);
-
-        logElemX.innerHTML = event.offsetX * kef  - 20;
-        logElemY.innerHTML = event.offsetY * kef - 47;
-
-        
-        
-        
-    }
-});
